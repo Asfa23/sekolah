13b941eb2aa1451049d9cc5371683a991f285775
diaz <52847109+diazarvinda@users.noreply.github.com>
2023-10-11 17:04:32 +0700

    Tampilan Pembayaran & Pembayaran

--- resources/views/lihat_pembayaran_siswa.blade.php
+++ resources/views/lihat_pembayaran_siswa.blade.php
@@ -5,39 +5,90 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lihat Pembayaran Siswa</title>
+    <!-- Include Tailwind CSS via CDN or import it using your build system -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
+    @vite('resources/css/app.css')
 </head>
 
-<body>
-    <h1>Data Pembayaran Siswa</h1>
+<body class="bg-gray-100">
+    <div class="flex">
+        <!-- Sidebar -->
+        <aside class="w-[30vh] h-[92vh] p-6 rounded-lg m-8 bg-gradient-to-t from-purple-600 to-purple-400 flex flex-col items-center space-y-10">         
+            <div class="w-40 h-40 bg-gradient-to-r from-purple-700 to-purple-500 rounded-2xl flex items-center justify-center mt-4">
+                <img src="img/student.svg" alt="Student Icon" class="w-20 h-20">
+            </div>
+        <ul class="space-y-4">        
+            <li>
+                <a href="#" class="text-white hover:underline flex items-center">
+                    <img src="img/i.svg" alt="About Icon" class="w-6 h-6 mr-2 font-bold">
+                    About
+                </a>
+            </li>
+            <li>
+                <a href="#" class="text-white hover:underline flex items-center">
+                    <img src="img/data.svg" alt="Payment Icon" class="w-6 h-6 mr-2 font-bold">
+                    Data Pembayaran
+                </a>
+            </li>
+            <li>
+                <a href="#" class="text-white hover:underline flex items-center">
+                    <img src="img/data.svg" alt="Payment Icon" class="w-6 h-6 mr-2 font-bold">
+                    Data Pengeluaran
+                </a>
+            </li>
+            <li>
+                <a href="#" class="text-white hover:underline flex items-center">
+                    <img src="img/report.svg" alt="Money Report Icon" class="w-6 h-6 mr-2 font-bold">
+                    Money Report
+                </a>
+            </li>
+            <li>
+                <a href="#" class="text-white hover:underline flex items-center">
+                    <img src="img/logout.svg" alt="Logout Icon" class="w-6 h-6 mr-2 font-bold">
+                    Logout
+                </a>
+            </li>
+        </ul>
+    </aside>
 
-    <table border="1">
-        <tr>
-            <th>ID Pembayaran</th>
-            <th>ID Siswa</th>
-            <th>Jumlah Pembayaran</th>
-            <th>Kategori</th>
-            <th>Tanggal Pembayaran</th>
-            <th>Aksi</th> <!-- Kolom untuk tombol aksi -->
-        </tr>
+        <!-- Main Content -->
+        <main class="w-3/4 p-8">
+            <h1 class="text-5xl font-bold mb-6">Data Pembayaran Siswa</h1>
 
-        @foreach ($pembayaranSiswa as $pembayaran)
-        <tr>
-            <td>{{ $pembayaran->ID_PEMBAYARAN }}</td>
-            <td>{{ $pembayaran->ID_SISWA }}</td>
-            <td>{{ $pembayaran->JUMLAH_PEMBAYARAN }}</td>
-            <td>{{ $pembayaran->KATEGORI }}</td>
-            <td>{{ $pembayaran->TANGGAL_PEMBAYARAN }}</td>
-            <td>
-                <a href="{{ url('/edit/'.$pembayaran->ID_PEMBAYARAN) }}">Edit</a>
-                <a href="{{ url('/delete/'.$pembayaran->ID_PEMBAYARAN) }}">Delete</a>
-            </td>
-        </tr>
-        @endforeach
-    </table>
+            <table class="w-full border border-collapse mb-8">
+                <thead>
+                    <tr>
+                        <th class="border p-2">ID Pembayaran</th>
+                        <th class="border p-2">ID Siswa</th>
+                        <th class="border p-2">Jumlah Pembayaran</th>
+                        <th class="border p-2">Kategori</th>
+                        <th class="border p-2">Tanggal Pembayaran</th>
+                        <th class="border p-2">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($pembayaranSiswa as $pembayaran)
+                    <tr>
+                        <td class="border p-2 text-center">{{ $pembayaran->ID_PEMBAYARAN }}</td>
+                        <td class="border p-2 text-center">{{ $pembayaran->ID_SISWA }}</td>
+                        <td class="border p-2 text-center">{{ $pembayaran->JUMLAH_PEMBAYARAN }}</td>
+                        <td class="border p-2 text-center">{{ $pembayaran->KATEGORI }}</td>
+                        <td class="border p-2 text-center">{{ $pembayaran->TANGGAL_PEMBAYARAN }}</td>
+                        <td class="border p-2 text-center">
+                            <a href="{{ url('/edit/'.$pembayaran->ID_PEMBAYARAN) }}"
+                                class="text-white bg-blue-500 hover:bg-blue-600 p-1 px-2 rounded-lg transition-colors duration-300">Edit</a>
+                            <a href="{{ url('/delete/'.$pembayaran->ID_PEMBAYARAN) }}"
+                                class="text-white bg-red-500 hover:bg-red-600 p-1 px-2 rounded-lg ml-2 transition-colors duration-300">Delete</a>
+                        </td>                            
+                    </tr>                    
+                    @endforeach
+                </tbody>
+            </table>
 
-    <a href="{{ url('/pembayaran') }}">Tambah Pembayaran</a>
-
+            <a href="{{ url('/pembayaran') }}"
+                class="bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600 focus:outline-none focus:bg-green-600">Tambah Pembayaran</a>
+        </main>
+    </div>
 </body>
 
 </html>
-
--- resources/views/tampilan_pembayaran.blade.php
+++ resources/views/tampilan_pembayaran.blade.php
@@ -4,31 +4,89 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tampilan Pembayaran</title>
+    <title>Pembayaran</title>
+    <!-- Include Tailwind CSS via CDN or import it using your build system -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
     @vite('resources/css/app.css')
 </head>
 
-<body>
-        <h1 class="bg-yellow-500">Form Pemesanan</h1>
+<body class="bg-gray-100">
+    <div class="flex">
+        <!-- Sidebar -->
+        <aside class="w-[30vh] h-[92vh] p-6 rounded-lg m-8 bg-gradient-to-t from-purple-600 to-purple-400 flex flex-col items-center space-y-10">         
+                <div class="w-40 h-40 bg-gradient-to-r from-purple-700 to-purple-500 rounded-2xl flex items-center justify-center mt-4">
+                    <img src="img/student.svg" alt="Student Icon" class="w-20 h-20">
+                </div>
+            <ul class="space-y-4">        
+                <li>
+                    <a href="#" class="text-white hover:underline flex items-center">
+                        <img src="img/i.svg" alt="About Icon" class="w-6 h-6 mr-2 font-bold">
+                        About
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="text-white hover:underline flex items-center">
+                        <img src="img/payment.svg" alt="Payment Icon" class="w-6 h-6 mr-2 font-bold">
+                        Pembayaran
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="text-white hover:underline flex items-center">
+                        <img src="img/report.svg" alt="Money Report Icon" class="w-6 h-6 mr-2 font-bold">
+                        Money Report
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="text-white hover:underline flex items-center">
+                        <img src="img/logout.svg" alt="Logout Icon" class="w-6 h-6 mr-2 font-bold">
+                        Logout
+                    </a>
+                </li>
+            </ul>
+        </aside>
 
-    <form action="/postPembayaran" method="POST">
-        @csrf 
-        <label for="ID_SISWA">ID Siswa:</label>
-        <input type="number" id="ID_SISWA" name="ID_SISWA" min="1000" required><br><br>
+        <!-- Main Content -->
+        <main class="w-3/5 p-8">
+            <h1 class="text-5xl font-bold mb-6">Pembayaran</h1>
 
-        <label for="JUMLAH_PEMBAYARAN">Jumlah Pembayaran:</label>
-        <input type="number" id="JUMLAH_PEMBAYARAN" name="JUMLAH_PEMBAYARAN" step="0.01" min="0" required><br><br>
+            <form action="/postPembayaran" method="POST" class="bg-white p-6 rounded shadow-md">
+                @csrf 
 
-        <label for="KATEGORI">Kategori:</label>
-        <select id="KATEGORI" name="KATEGORI">
-            <option value="Pembayaran Siswa">Pembayaran Siswa</option>
-        </select><br><br>
-        
-        <label for="TANGGAL_PEMBAYARAN">Tanggal Pembayaran:</label>
-        <input type="date" id="TANGGAL_PEMBAYARAN" name="TANGGAL_PEMBAYARAN" required><br><br>
-        
-        <button type="submit">Submit</button>
-    </form>
+            <div class="mb-4">
+                <label for="ID_SISWA" class="block text-sm font-medium text-gray-700">ID Siswa:</label>
+                <input type="number" id="ID_SISWA" name="ID_SISWA" min="1000" required
+                    class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500">
+            </div>
+
+            <div class="mb-4">
+                <label for="JUMLAH_PEMBAYARAN" class="block text-sm font-medium text-gray-700">Jumlah Pembayaran:</label>
+                <input type="number" id="JUMLAH_PEMBAYARAN" name="JUMLAH_PEMBAYARAN" step="0.01" min="0" required
+                    class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500">
+            </div>
+
+            <div class="mb-4">
+                <label for="KATEGORI" class="block text-sm font-medium text-gray-700">Kategori:</label>
+                <select id="KATEGORI" name="KATEGORI"
+                    class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500">
+                    <option value="Pembayaran Siswa">Pembayaran Siswa</option>
+                </select>
+            </div>
+
+            <div class="mb-4">
+                <label for="TANGGAL_PEMBAYARAN" class="block text-sm font-medium text-gray-700">Tanggal Pembayaran:</label>
+                <input type="date" id="TANGGAL_PEMBAYARAN" name="TANGGAL_PEMBAYARAN" required
+                    class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500">
+            </div>
+
+                <div class="mt-6">
+                    <button type="submit"
+                        class="w-full py-2 px-4 bg-green-500 text-white rounded-lg hover:bg-green-600 focus:outline-none focus:bg-green-600">
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </main>
+    </div>
 </body>
 
 </html>
