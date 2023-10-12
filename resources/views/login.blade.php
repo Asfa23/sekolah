4b20813b8a467ffb85d02841ce6e2650094f95a7
Asfa23 <112614008+Asfa23@users.noreply.github.com>
2023-10-12 18:03:28 +0700

    update responsif dikit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,9 +11,9 @@
 </head>
 <body class="bg-gray-100 font-poppins">
     <div class="flex h-screen">
-        <div class="w-[55%] p-10">
-            <h1 class="text-6xl font-bold text-black mb-12 ml-16 mt-20">Welcome!</h1>
-            <div class="w-[40vw] h-[50vh] bg-white rounded-lg shadow-md p-10 ml-16">
+        <div class="w-[60%] p-10 flex flex-col justify-center">
+            <h1 class="text-6xl font-bold text-black mb-7 ml-16">Welcome!</h1>
+            <div class="w-[40vw] bg-white rounded-xl shadow-md p-10 ml-16">
                 <h2 class="text-3xl font-semibold text-black mb-4 mt-1">User Login</h2>
                 <div class="flex flex-col items-center mt-8">
                     <div class="w-full mb-4">
@@ -22,7 +22,7 @@
                     <div class="w-full mb-4">
                         <input class="w-full text-xl bg-gray-200 rounded-lg p-4" type="password" placeholder="Password">
                     </div>
-                    <a href="/about" class="w-full h-[7vh] bg-black text-white font-semibold rounded-lg mb-4 mt-6 flex items-center justify-center">
+                    <a href="/about" class="w-full h-[7vh] bg-black text-white font-semibold rounded-lg mt-6 flex items-center justify-center">
                         Login
                     </a>                                   
                 </div>
