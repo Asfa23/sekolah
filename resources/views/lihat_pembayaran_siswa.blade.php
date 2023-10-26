1c00bf12af0261c353230d8af805c1d2a055d120
diaz <52847109+diazarvinda@users.noreply.github.com>
2023-10-26 19:20:55 +0700

    update

--- resources/views/lihat_pembayaran_siswa.blade.php
+++ resources/views/lihat_pembayaran_siswa.blade.php
@@ -125,7 +125,9 @@
             <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
             <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"> 
                 <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <img id="modalImage" src="" alt="Image" class="w-full">
+                    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                        <img id="modalImage" src="" alt="Image" class="w-full max-h-screen">
+                    </div>                    
                 </div>
                 <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                     <button onclick="closeModal()" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover-bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:mt-0 sm:w-auto sm:text-sm">
